04bef9c976d0c3c54c3a1a1bfc5f2d39a3b0fb16
Anderson <Anderson@Anderson-PC>
2013-10-09 13:02:08 -0300

    Iniciando as Telas -SplashScren -Pergunta

--- src/com/ads/proplan/MainActivity.java
+++ src/com/ads/proplan/MainActivity.java
@@ -1,15 +1,52 @@
 package com.ads.proplan;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.os.Bundle;
+import android.provider.ContactsContract.CommonDataKinds.Im;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+	private ImageView iconeImageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		iconeImageView = (ImageView) findViewById(R.id.splash_image_icone);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		timeSplash(2000);
+	}
+
+	/**
+	 * O metodo Time splash da classe MainActivity, é uma metodo void que tem o
+	 * objetivo de criar um processo paralelo para contar um tempo que acionara
+	 * uma noma activity e executara o finish nesta.
+	 * 
+	 * @param time
+	 *            - O parametro time é do tipo int. Ele determina o tempo em
+	 *            milesegundos
+	 */
+	private void timeSplash(int time) {
+		new Timer().schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+				finish();
+				Intent intent = new Intent();
+				// intent.setClass(SplashScreens.this, MenuActivity.class);
+				// startActivity(intent);
+			}
+		}, time);
 	}
 
 	@Override
